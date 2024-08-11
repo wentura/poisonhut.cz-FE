617aa8f923a1b92409992004e3f36f23974046b9
Zbynek Svoboda <wentura17@seznam.cz>
2024-08-11 11:41:30 +0200

    blurry bg

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex"></div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-green-700 before:dark:opacity-40 after:dark:from-sky-900 after:dark:via-green-300 after:dark:opacity-60 before:lg:h-[360px] z-[-1] text-4xl font-extrabold">
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full  before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:blur-2xl after:content-[''] before:bg-gradient-to-br before:from-transparent before:to-green-700 before:opacity-40 after:from-sky-500 after:via-green-200 after:opacity-60 before:lg:h-[560px] z-[-1] text-4xl font-extrabold">
         PoisonHut.cz
       </div>
 
